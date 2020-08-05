907ed2459f76e362f28f52f18db0a40bed80edb1
Jon Charter <jmcharter@gmail.com>
2020-08-05 08:42:08 +0100

    user login / logout logic

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,14 @@
-from app import app
-from flask import render_template, flash, redirect, url_for
+from app import app, db
+from flask import render_template, flash, redirect, url_for, request
+from flask_login import current_user, login_user, logout_user, login_required
 from app.forms import LoginForm
-from flask_login import current_user, login_user
 from app.models import User
+from werkzeug.urls import url_parse
 
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
     return render_template('index.html', title='Donut Tracker')
 
@@ -19,5 +21,16 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Incorrect username or password.')
-            return redirect(url_for('index'))
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_parse('index')
+        return redirect(next_page)
     return render_template('login.html', title='Log in', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
