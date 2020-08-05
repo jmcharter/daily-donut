fb440a508c53dc65f0ea41d1428d62206e91d52d
Jon Charter <jmcharter@gmail.com>
2020-08-05 08:42:58 +0100

    import werkzeug > import werkzeug.security

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from app import db, login
-from werkzeug import generate_password_hash, check_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
